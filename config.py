f817495090758901bed96f5f33be11e6182eb725
Rulf <lazaarx9@gmail.com>
2026-07-10 08:39:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "5916831174:AAHuwX__sB9jeSjyW9VpIZAMIg2e0nlybm8"
+OWNER_ID = 5179011789
+TELEGRAM_API = 25918874
+TELEGRAM_HASH = "87c7c525932cf3d753bea33786ad71ee"
+DATABASE_URL = "mongodb+srv://WZMLX:WZMLX@cluster0.r9zqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+UPSTREAM_REPO = "https://github.com/cmd-rulf/WZv3-MOD"
+UPSTREAM_BRANCH = "wzv3"
+BASE_URL = "http://178.62.124.42" 
